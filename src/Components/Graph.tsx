47a86ba5e3c8f0c258382ff4c9747518397c0e55
himarnoel <olaimarnoel@gmail.com>
2023-06-13 00:31:27 +0100

    added a new file

--- src/Components/Graph.tsx
+++ src/Components/Graph.tsx
@@ -2,7 +2,6 @@ import { ArcElement, Chart } from "chart.js";
 import React, { FC } from "react";
 import { Doughnut } from "react-chartjs-2";
 const Graph: FC = () => {
-  
   Chart.register(ArcElement);
   const config = {
     data: {
@@ -15,22 +14,25 @@ const Graph: FC = () => {
             "rgb(255, 205, 86)",
           ],
           hoverOffset: 4,
-          borderRadius:30,
-          spacing:10
+          borderRadius: 30,
+          spacing: 10,
         },
       ],
     },
-    options:{
-        cutout:115
-    }
-
+    options: {
+      cutout: 115,
+    },
   };
 
   return (
     <div className="flex justify-content max-w-xs mx-auto">
       <div className="item">
         <div className="chart relative">
-          <Doughnut {...config}/>
+          <Doughnut {...config} />
+          <h3 className="mb-4 font-bold title">
+            Total
+            <span className="block text-3xl text-emerald-400">${0}</span>
+          </h3>
         </div>
       </div>
       <div className="flex flex-col py-10 gap-4"></div>
---
+++ src/Components/Labels.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const Labels: FC = () => {
+  return <div className=""></div>;
+};
+
+export default Labels;
