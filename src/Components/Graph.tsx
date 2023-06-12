840227fa8dac637a5761929289c834297639dc32
himarnoel <olaimarnoel@gmail.com>
2023-06-13 00:05:04 +0100

    cool

--- src/Components/Graph.tsx
+++ src/Components/Graph.tsx
@@ -1,15 +1,15 @@
-import React, { FC } from 'react'
-
-const Graph:FC = () => {
+import { Chart } from "chart.js";
+import React, { FC } from "react";
+import { Doughnut } from "react-chartjs-2";
+const Graph: FC = () => {
   return (
-    <div className='flex justify-content max-w-xs mx-auto'>
-        <div className="item">
-            <div className="chart relative">
-                chart
-            </div>
-        </div>
+    <div className="flex justify-content max-w-xs mx-auto">
+      <div className="item">
+        <div className="chart relative">chart</div>
+      </div>
+      <div className="flex flex-col py-10 gap-4"></div>
     </div>
-  )
-}
+  );
+};
 
-export default Graph
+export default Graph;
