57ba12d9da5ca2ec028cbfcf275fe71110689cf1
himarnoel <olaimarnoel@gmail.com>
2023-06-12 23:46:57 +0100

    created a new file

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,13 @@ function App() {
 
   return (
    <div className="App">
-    <div className="container mx-auto max-w-6xl">
-      <h1>Expense Tracker</h1>
+    <div className="container mx-auto max-w-6xl shadow-lg">
+      <h1 className='text-4xl py-8 mb-10 bg-slate-800 text-white rounded'>Expense Tracker</h1>
      
+    {/* grid cols */}
+    <div className="grid md:grid-cols-2 gap-4"></div>
+
+    {/* Graphs */}
     
     </div>
    </div>
