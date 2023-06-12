8e90cd6a0a6e4c8499c69e3d764d2924e091b2ae
himarnoel <olaimarnoel@gmail.com>
2023-06-13 00:11:43 +0100

    chart now working

--- src/Components/Graph.tsx
+++ src/Components/Graph.tsx
@@ -1,11 +1,29 @@
-import { Chart } from "chart.js";
+import { ArcElement, Chart } from "chart.js";
 import React, { FC } from "react";
 import { Doughnut } from "react-chartjs-2";
 const Graph: FC = () => {
+  const data = {
+    labels: ["Red", "Blue", "Yellow"],
+    datasets: [
+      {
+        label: "My First Dataset",
+        data: [300, 50, 100],
+        backgroundColor: [
+          "rgb(255, 99, 132)",
+          "rgb(54, 162, 235)",
+          "rgb(255, 205, 86)",
+        ],
+        hoverOffset: 4,
+      },
+    ],
+  };
+  Chart.register(ArcElement);
   return (
     <div className="flex justify-content max-w-xs mx-auto">
       <div className="item">
-        <div className="chart relative">chart</div>
+        <div className="chart relative">
+          <Doughnut data={data} />
+        </div>
       </div>
       <div className="flex flex-col py-10 gap-4"></div>
     </div>
