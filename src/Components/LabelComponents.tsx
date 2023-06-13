d614303e0a92894f1389d54e4e4015e4b95c2a50
himarnoel <olaimarnoel@gmail.com>
2023-06-13 01:11:02 +0100

    writing cool code

--- src/Components/LabelComponents.tsx
+++ src/Components/LabelComponents.tsx
@@ -6,15 +6,18 @@ interface LabelComponentsProps {
 }
 
 const LabelComponents: FC<LabelComponentsProps> = ({ data }) => {
+  if (!data) return <div></div>;
   return (
-    <div className="label flex justify-between">
+    <div className="labels flex justify-between">
       <div className="flex gap-2">
         <div
-          className={`"w-2 h-3 rounded py-3 ${data.percent ?? "bg-[#f9c74f]"}"`}
+          className={`w-2 h-3 rounded py-3 ${
+            `bg-[${data.color}]` ?? "bg-[#f9c74f]"
+          }`}
         ></div>
-        <h3>{data.type ?? "Savings"}</h3>
+        <h3>{data.type ?? ""}</h3>
       </div>
-      <h3 className="font-bold">{data.percent ?? "45%"}</h3>
+      <h3 className="font-bold">{data.percent ?? 0}%</h3>
     </div>
   );
 };
