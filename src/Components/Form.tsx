e3bc5c26f21e735b0f0043c2beb53ecdabbab495
himarnoel <olaimarnoel@gmail.com>
2023-06-13 01:27:40 +0100

    working

--- src/Components/Form.tsx
+++ src/Components/Form.tsx
@@ -3,8 +3,35 @@ import { FC } from "react";
 interface FormProps {}
 
 const Form: FC<FormProps> = () => {
-  return <>
-  form</>;
+  return (
+    <>
+      <div className="form max-w-sm mx-auto w-96">
+        <h1 className="font-bold pb-4 text-xl">Transaction</h1>
+        <form action="" id="form">
+          <div className="grid gap-4">
+            <div className="input-group">
+              <input
+                type="text"
+                placeholder="Salary, House Rent, SIP"
+                className="form-input"
+              />
+              <select id="" className="form-input">
+                <option value="Investment" defaultValue="Investment">
+                  Investment
+                </option>
+                <option value="Investment" defaultValue="Investment">
+                  Expense
+                </option>{" "}
+                <option value="Investment" defaultValue="Investment">
+                  Savings
+                </option>
+              </select>
+            </div>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default Form;
