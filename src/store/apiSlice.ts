f6c73496071fe7f403f072bb4416806438972749
Olaleye <tolaleyeakin@gmail.com>
2023-12-22 12:22:19 +0100

    working on fetching data

---
+++ src/store/apiSlice.ts
@@ -0,0 +1,10 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query'
+const baseUri="http://localhost:8080"
+export const apiSlice=createApi({
+    baseQuery:fetchBaseQuery({baseUrl:baseUri}),
+    endpoints:builder=>({
+        getCategories:builder.query({
+            query:()=>'/api/categories'
+        })
+    })
+})
